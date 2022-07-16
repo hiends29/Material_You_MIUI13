0714108bb79d18633cb5e325c4b9e8f90b21116c
buihien224 <buihien224@gmail.com>
2022-07-16 19:19:49 +0700

    done

---
+++ make.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+recp="java -jar bin/apktool.jar"
+
+if [[ -d apk_out ]]; then
+	rm -rf apk_out/*
+else mkdir apk_out
+fi
+
+$recp if bin/*apk
+
+for i in $(ls source); do
+	echo "Recomplie : $i"
+	$recp b source/$i -o "apk_out/$i.apk"
+	if [[ -f "apk_out/$i.apk" ]]; then
+		echo "Signing : $i"
+		apksigner sign --ks bin/miuivs --ks-pass pass:linkcute "apk_out/$i.apk"
+		rm -rf "apk_out/$i.apk.idsig"
+	else
+		echo "$i : Failure"
+	fi
+done
+
+if [ -z "$(ls -A apk_out)" ]; then
+   echo "Empty output"
+else
+   echo "Making magisk module"
+   cp -rf module module_temp
+   cp apk_out/*.apk module_temp/system/product/overlay
+   sed -i "s@abcxyz@$(date +"%H%M%d%m")@g" module_temp/module.prop
+   7za a -tzip "$1_$(date +"%H%M%d%m")_MTYM13.zip" module_temp/.
+   rm -rf module_temp
+fi
+
+

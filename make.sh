8d5471878b6946974144505041fb20c1b89b4214
Bùi Đức Hiền <54226198+buihien224@users.noreply.github.com>
2022-07-16 19:32:21 +0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -26,10 +26,10 @@ if [ -z "$(ls -A apk_out)" ]; then
 else
    echo "Making magisk module"
    cp -rf module module_temp
-   cp -rf apk_out/. module_temp/system/product/overlay/
-   sed -i "s@abcxyz@$(date +"%H%M%d%m")@g" module_temp/module.prop
-   7za a -tzip "$1_$(date +"%H%M%d%m")_MTYM13.zip" module_temp/.
-   rm -rf module_temp
+   sudo cp -rf apk_out/. module_temp/system/product/overlay/
+   sudo sed -i "s@abcxyz@$(date +"%H%M%d%m")@g" module_temp/module.prop
+   sudo 7za a -tzip "$1_$(date +"%H%M%d%m")_MTYM13.zip" module_temp/.
+   sudo rm -rf module_temp
 fi
 
 
